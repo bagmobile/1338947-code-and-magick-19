7102599b5f463b16056517811899fcdacbbe145d
Beloy Andrey <bagmobile@yandex.ru>
2020-02-12 13:32:29 +0300

    11. Учебный проект: в движении. Перемещение диалога

--- js/dialog.js
+++ js/dialog.js
@@ -26,21 +26,28 @@
     '#e6e848',
   ];
 
-  var setup = document.querySelector('.setup');
+  var setupDialogElement = document.querySelector('.setup');
   var setupOpen = document.querySelector('.setup-open');
-  var setupClose = setup.querySelector('.setup-close');
-  var setupUserName = setup.querySelector('.setup-user-name');
+  var setupClose = setupDialogElement.querySelector('.setup-close');
+  var setupUserName = setupDialogElement.querySelector('.setup-user-name');
   var setupWizardCoat = document.querySelector('.setup-wizard .wizard-coat');
   var setupWizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
   var setupFireball = document.querySelector('.setup-fireball-wrap');
+  var dialogHandler = setupDialogElement.querySelector('.upload');
+  var initChordsSetupDialogElement = {
+    x: setupDialogElement.offsetLeft,
+    y: setupDialogElement.offsetTop,
+  };
 
   var openPopup = function () {
-    setup.classList.remove('hidden');
+    setupDialogElement.style.top = initChordsSetupDialogElement.y + 'px';
+    setupDialogElement.style.left = initChordsSetupDialogElement.x + 'px';
+    setupDialogElement.classList.remove('hidden');
     document.addEventListener('keydown', onPopupEscPress);
   };
 
   var closePopup = function () {
-    setup.classList.add('hidden');
+    setupDialogElement.classList.add('hidden');
     document.removeEventListener('keydown', onPopupEscPress);
   };
 
@@ -83,5 +90,53 @@
     evt.currentTarget.style.background = color;
     evt.currentTarget.querySelector('input[name="fireball-color"]').value = color;
   });
+
+  dialogHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY,
+    };
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY,
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY,
+      };
+
+      setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
+      setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          dialogHandler.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandler.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
 
 })();
